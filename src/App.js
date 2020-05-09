47d264705dadeb9d1368cac989f16a6dca3f1421
TheVinh <binkool281132442@gmail.com>
2020-05-09 11:55:19 +0700

    upload my introduce website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from "react-router-dom"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 import Home from './components';
@@ -10,12 +10,14 @@ import Contact from "./components/Contact"
 function App() {
   return (
     <>
+    <Router>
     <CssBaseline />
       <Route exact path="/" component={Home}/>
       <Route path="/my-resume" component={Resume}/>
       <Route path="/my-target" component={Portfolio}/>
       <Route path="/contacts" component={Contact}/>
       <Route path="/I-troduce" component={Alone}/>
+      </Router>
     </>
   );
 }
