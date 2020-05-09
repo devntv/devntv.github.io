20674025d9aa5dc0c0bc48ff6c73781d0c86541c
TheVinh <binkool281132442@gmail.com>
2020-05-09 12:54:17 +0700

    Updates

---
+++ precache-manifest.763b2a983832d86a033713ba46bfaafb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb37aaee63088188fbb05af23f2a57aa",
+    "url": "/devntv.github.io/index.html"
+  },
+  {
+    "revision": "4b830e8661a195e6d127",
+    "url": "/devntv.github.io/static/css/main.9323182c.chunk.css"
+  },
+  {
+    "revision": "308a1dd25745310e48d0",
+    "url": "/devntv.github.io/static/js/2.ffa49f67.chunk.js"
+  },
+  {
+    "revision": "e4279675a9c60179159c82343fcf3639",
+    "url": "/devntv.github.io/static/js/2.ffa49f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b830e8661a195e6d127",
+    "url": "/devntv.github.io/static/js/main.b923c7a7.chunk.js"
+  },
+  {
+    "revision": "b85980cc3b30f4917734",
+    "url": "/devntv.github.io/static/js/runtime-main.e549858d.js"
+  },
+  {
+    "revision": "fce168e61c31d0431e891b80ea4f6284",
+    "url": "/devntv.github.io/static/media/avatar1.fce168e6.jpg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/devntv.github.io/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/devntv.github.io/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/devntv.github.io/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/devntv.github.io/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/devntv.github.io/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "20862f7e531c31fc7da34809c8192f15",
+    "url": "/devntv.github.io/static/media/w1.20862f7e.jpg"
+  }
+]);
